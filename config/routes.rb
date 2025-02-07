253feb3571cd7bc9fb32c9ee1e7c6bdd14a9624e
sakyaaa <dima9154481610@yandex.ru>
2025-02-07 14:21:40 +0000

    authorization using devise

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,7 @@ Rails.application.routes.draw do
   get 'up' => 'rails/health#show', as: :rails_health_check
 
   # Defines the root path route ("/")
-  # root "posts#index"
+  root 'posts#index'
+
+  devise_for :users
 end
