fa7952f36144d45543b160768759f96039b3bbcc
sakyaaa <dima9154481610@yandex.ru>
2025-03-12 10:41:52 +0000

    Added search function for articles

--- config/routes.rb
+++ config/routes.rb
@@ -8,10 +8,13 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get 'up' => 'rails/health#show', as: :rails_health_check
 
+  # User accounts
   devise_for :user
 
+  # User profiles
   resources :users, only: %i[index show]
 
+  # Articles and comments
   resources :articles do
     resources :comments, only: %i[index create destroy]
   end
